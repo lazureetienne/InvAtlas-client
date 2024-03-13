fc56ae27cb0bb695ce3c0721c9a81d6f5791d278
lazureetienne <47014155+lazureetienne@users.noreply.github.com>
2024-03-13 18:53:40 -0400

    Added Level bar

--- app/src/main/java/com/example/invatlas/MainActivity.kt
+++ app/src/main/java/com/example/invatlas/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,8 +24,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.invatlas.ui.theme.InvAtlasTheme
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -32,6 +38,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // TODO: Fetch user from server.
         setContent {
             var selectedItem by remember { mutableIntStateOf(0) }
             val items = listOf(
@@ -78,6 +85,33 @@ class MainActivity : ComponentActivity() {
         @DrawableRes val selectedIconId: Int
     )
 
+    private val levels = createLevels()
+
+    private val currentUser = createDummyUser()
+    private fun createLevels(): List<Level> {
+        val levels = mutableListOf<Level>()
+        for (i in 1..100) {
+            levels.add(Level(i, 10 * i))
+        }
+        return levels
+    }
+
+    private fun createDummyUser(): User {
+        return User(
+            "testAuth",
+            "testUser",
+            5
+        )
+    }
+
+    private fun getUserLevel(user: User): Int {
+        val userLevel = levels.find { it.xpCap > user.xp } ?: levels.last()
+        return userLevel.level
+    }
+
+    private fun xpCap(userLevel: Int): Int {
+        return levels.find { it.level == userLevel }?.xpCap ?: 0
+    }
 
     @Composable
     fun AtlasScreen() {
@@ -85,9 +119,37 @@ class MainActivity : ComponentActivity() {
         val cameraPositionState = rememberCameraPositionState {
             position = CameraPosition.fromLatLngZoom(singapore, 10f)
         }
+        Box(
+            modifier = Modifier
+                .height(40.dp)
+                .fillMaxWidth()
+        ) {
+            Row(
+                modifier = Modifier.align(Alignment.Center),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier.padding(end = 15.dp),
+                    text = "Niveau ${getUserLevel(currentUser)}"
+                )
+                LinearProgressIndicator(
+                    progress = currentUser.xp.toFloat() / xpCap(getUserLevel(currentUser)),
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .height(20.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                )
+                Text(
+                    modifier = Modifier.padding(start = 15.dp),
+                    text = "${currentUser.xp}/${xpCap(getUserLevel(currentUser))} xp"
+                )
+            }
+        }
         GoogleMap(
             modifier = Modifier
-                .fillMaxSize(),
+                .fillMaxSize()
+                .padding(bottom = 70.dp, top = 40.dp),
             cameraPositionState = cameraPositionState
         ) {
             // TODO
@@ -103,6 +165,7 @@ class MainActivity : ComponentActivity() {
     fun FloradexScreen() {
         Text(text = "Floradex")
     }
+
     @Preview(showBackground = true)
     @Composable
     fun Preview() {
