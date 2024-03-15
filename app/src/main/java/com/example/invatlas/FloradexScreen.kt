5dad638c0474601041f22864a49844d855d1043b
william <william_martineau@hotmail.com>
2024-03-15 00:13:38 -0400

    changes

--- app/src/main/java/com/example/invatlas/FloradexScreen.kt
+++ app/src/main/java/com/example/invatlas/FloradexScreen.kt
@@ -9,9 +9,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -26,25 +28,30 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.invatlas.viewmodels.PlantViewModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun FloradexScreen(vm: PlantViewModel) {
-    Text(text = "Floradex")
-
     LaunchedEffect(Unit, block = {
         vm.getAllPlants()
     })
 
     LazyVerticalGrid(
-        columns = GridCells.Adaptive(minSize = 128.dp)
+        columns = GridCells.Adaptive(minSize = 192.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 40.dp, bottom = 90.dp)
     ) {
         items(vm.plantList.size) { plant ->
             Card(
@@ -52,21 +59,23 @@ fun FloradexScreen(vm: PlantViewModel) {
                     containerColor = MaterialTheme.colorScheme.surfaceVariant,
                 ),
                 modifier = Modifier
-                    .size(width = 240.dp, height = 100.dp)
+                    .size(width = 240.dp, height = 128.dp)
+                    .padding(5.dp)
             ) {
+                AsyncImage(
+                    model = "http://192.168.0.190:5000/reference/" + vm.plantList[plant].imgPath,
+                    contentDescription = "Hello guys",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(0.75f)
+                        .align(Alignment.CenterHorizontally),
+                    contentScale = ContentScale.Crop,
+                )
                 Text(
                     text = vm.plantList[plant].name,
                     modifier = Modifier
                         .padding(4.dp),
                     textAlign = TextAlign.Center,
-                )
-                println(vm.plantList[plant].imgPath)
-                AsyncImage(
-                    model = "http://192.168.0.190:5000/reference/" + vm.plantList[plant].imgPath,
-                    contentDescription = "Hello guys",
-                    modifier = Modifier
-                        .aspectRatio(1f)
-                        .requiredSize(200.dp)
                 )
             }
 
